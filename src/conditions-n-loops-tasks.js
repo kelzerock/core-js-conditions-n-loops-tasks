b1d0229a5b1071d1d86e0809851c2047c52b2f9f
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-12-01 22:18:25 +0300

    feat: complete sortByAsc with error in speed

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -497,8 +497,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const arrClone = arr;
+  for (let i = 1; i < arrClone.length; i += 1) {
+    if (arrClone[i] < arrClone[i - 1]) {
+      let j = i;
+      while (arrClone[j] < arrClone[j - 1]) {
+        [arrClone[j], arrClone[j - 1]] = [arrClone[j - 1], arrClone[j]];
+        j -= 1;
+      }
+    }
+  }
+  return arr;
 }
 
 /**
